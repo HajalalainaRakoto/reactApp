aca40620ea9e7c179814cb6cd1b4fd358f4ba7a1
MikeYannick <nickkise27@gmail.com>
2022-05-04 10:33:13 +0300

    ToDoInput

--- src/index.js
+++ src/index.js
@@ -7,11 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -25,4 +21,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-registerServiceWorker();
