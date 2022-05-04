fc5dad73e6bb2ce470f7d79cf4b4f9669eb69e1f
MikeYannick <nickkise27@gmail.com>
2022-05-04 14:31:13 +0300

    réctification App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div class="container">
-    <div class="text-center border border-info p-3 mt-2" style={{backgroundcolor: "#eee;"}}>
+    <div className="mx-auto text-center border border-info mt-2 p-3" style= {{width: "700px", backgroundcolor: "#eee;"}}>
       <h3>To Do Input</h3>
-      <form  class="ml-5 mb-3 mt-5">
-        <div class="input-group rounded border border-primary">
-          <div class="input-group-append">
-            <button class="btn btn-primary" type="button">
+        <div className="input-group rounded border border-primary mt-5">
+          <div className="input-group-append">
+            <button className="btn btn-primary" type="button">
               @
             </button>
           </div>
-          <input type="text" class="form-control bg-light border-0 small" placeholder="New To Do"></input>
+          <input type="text" className="form-control bg-light border-0 small" placeholder="New To Do"></input>
         </div>
-          <button type="submit" class="btn btn-primary text-center mt-3 w-75">Add new task</button>
-      </form>
+        <div class="mb-3">
+          <button type="submit" className="btn btn-primary text-center mt-3 w-100">Add new task</button>
+        </div>
     </div>
-  </div>
 
   );
 }
