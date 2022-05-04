bb9b9413098b15f8c5e7282770685166a4fd074f
MikeYannick <nickkise27@gmail.com>
2022-05-04 12:30:03 +0300

    réinitialisation index.html; modification App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+    <div class="text-center border border-info p-3 mt-2" style={{backgroundcolor: "#eee;"}}>
+      <h3>To Do Input</h3>
+      <form  class="ml-5 mb-3 mt-5">
+        <div class="input-group rounded border border-primary">
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="button">
+              @
+            </button>
+          </div>
+          <input type="text" class="form-control bg-light border-0 small" placeholder="New To Do"></input>
+        </div>
+          <button type="submit" class="btn btn-primary text-center mt-3 w-75">Add new task</button>
+      </form>
     </div>
+  </div>
+
   );
 }
 
